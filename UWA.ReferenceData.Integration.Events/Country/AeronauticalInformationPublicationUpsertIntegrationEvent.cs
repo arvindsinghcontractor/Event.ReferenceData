e83f547b64c93086165f6befae08bc9686d6a2fc
DESKTOP-LNQST10\DESKTOP 1 <arsingh@UniversalWeather.NET>
2022-10-03 17:36:34 +0530

    Push Initial Commit

---
+++ UWA.ReferenceData.Integration.Events/Country/AeronauticalInformationPublicationUpsertIntegrationEvent.cs
@@ -0,0 +1,11 @@
+﻿namespace UWA.ReferenceData.Integration.Events.Country
+{
+    public class AeronauticalInformationPublicationUpsertIntegrationEvent
+    {
+        public int AeronauticalInformationPublicationId { get; set; }
+        public string AIPLink { get; set; }
+        public string AIPUsername { get; set; }
+        public string AIPPassword { get; set; }
+        public string Description { get; set; }
+    }
+}
---
+++ UWA.ReferenceData.Integration.Events/Permit/BlanketValidityTypeUpsertIntegrationEvent.cs
@@ -0,0 +1,8 @@
+﻿namespace UWA.ReferenceData.Integration.Events.Permit
+{
+    public class BlanketValidityTypeUpsertIntegrationEvent
+    {
+        public int BlanketValidityTypeId { get; set; }
+        public string Name { get; set; }
+    }
+}
